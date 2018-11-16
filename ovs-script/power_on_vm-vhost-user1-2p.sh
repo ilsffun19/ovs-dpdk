b4a50e31a472624798ccdf3b31a4374dcf0770d1
ilsffun19 <irene.liew@intel.com>
2018-11-16 10:40:01 -0700

    edited the power on VM commands

--- ovs-script/power_on_vm-vhost-user1-2p.sh
+++ ovs-script/power_on_vm-vhost-user1-2p.sh
@@ -9,7 +9,7 @@ then
     echo "VM $vm not found!"
 else
     echo "VM $vm started! VNC: $vnc, ssh port: 2024"
-    taskset -c 8-10 /root/ovs-dpdk/qemu-2.12.1/x86_64-softmmu/qemu-system-x86_64 -m 4G -smp 3,cores=3,threads=1,sockets=1 -cpu host -hda $vm  -boot c -enable-kvm -name $vm_name \
+    taskset -c 8-10 /root/ovs-dpdk/qemu-2.12.1/x86_64-softmmu/qemu-system-x86_64 -m 4G -smp 3,cores=3,threads=1,sockets=1 -cpu host -drive format=raw,file=$vm -boot c -enable-kvm -name $vm_name \
 -object memory-backend-file,id=mem,size=4G,mem-path=/dev/hugepages,share=on -numa node,memdev=mem -mem-prealloc \
 -netdev user,id=nttsip,hostfwd=tcp::2024-:22 \
 -device e1000,netdev=nttsip \
--- ovs-script/power_on_vm-vhost-user2-2p.sh
+++ ovs-script/power_on_vm-vhost-user2-2p.sh
@@ -9,7 +9,7 @@ then
     echo "VM $vm not found!"
 else
     echo "VM $vm started! VNC: $vnc, SSH Port: 2025"
-    taskset -c 11-13 /root/ovs-dpdk/qemu-2.12.1/x86_64-softmmu/qemu-system-x86_64 -m 4096 -smp 3,cores=3,threads=1,sockets=1 -cpu host -hda $vm  -boot c -enable-kvm -name $vm_name \
+    taskset -c 11-13 /root/ovs-dpdk/qemu-2.12.1/x86_64-softmmu/qemu-system-x86_64 -m 4096 -smp 3,cores=3,threads=1,sockets=1 -cpu host -drive format=raw,file=$vm -boot c -enable-kvm -name $vm_name \
 -object memory-backend-file,id=mem,size=4096M,mem-path=/dev/hugepages,share=on -numa node,memdev=mem -mem-prealloc \
 -netdev user,id=nttsip,hostfwd=tcp::2025-:22 \
 -device e1000,netdev=nttsip \
