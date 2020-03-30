e7c41d4eb256ed24af241d8ec64e65af46c7a0a1
Irene <ilsffun19@users.noreply.github.com>
2020-03-30 11:24:35 -0700

    Update compile_qemu.sh

--- compile_qemu.sh
+++ compile_qemu.sh
@@ -1,5 +1,7 @@
-cd /root/ovs-dpdk/qemu-2.12.1/
+cd /root/ovs-dpdk/qemu-4.2.0/
 apt-get install -y  libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev
 ./configure --target-list=x86_64-softmmu
 make -j10
+make install
+
 
