d760f1fd8f76513caa665a6dbec65ce1f0c1ecc7
Irene <ilsffun19@users.noreply.github.com>
2020-03-30 12:20:37 -0700

    Update setup_ovs.sh

--- setup_ovs.sh
+++ setup_ovs.sh
@@ -1,9 +1,9 @@
 mkdir -p /root/ovs-dpdk/
 cd /root/ovs-dpdk  
-echo "Installing DPDK 19.11..."
-wget https://fast.dpdk.org/rel/dpdk-19.11.tar.xz
-tar xvf dpdk-19.11.tar.xz
-ln -rs -v dpdk-stable-19.11 dpdk
+echo "Installing DPDK 18.11.6 ..."
+wget https://fast.dpdk.org/rel/dpdk-18.11.6.tar.xz
+tar xvf dpdk-18.11.6.tar.xz
+ln -rs -v dpdk-stable-18.11.6 dpdk
 ./compile_dpdk.sh 
 echo "Done"
 echo "Installing OVS-2.12.0..."
